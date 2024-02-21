a510a4c0718ab9d986d14872f638a53706fb6e64
mathewan10y <mathewan10y@gmail.com>
2024-02-21 11:58:01 +0530

    inheritance

--- clss/bin/clss.dart
+++ clss/bin/clss.dart
@@ -1,5 +1,8 @@
 // final,const,class,object,constructor,named constructor,
 // public memeber,private member
+//inheritance,override,super,abstract class,mixins(used for muliple inheritance)
+//implements (for abstract classes) ,extends (for normal classes), with (for mixins)
+//interface (using abstract class and implement)
 
 /*void main() {
   final person = Person();
@@ -22,6 +25,11 @@ void main() {
 
   person
       .sayhai(); // sayhai fuction exists for person as it is a public member and and object can have that function whereas Person.sayhai() results in an error
+
+  final human = Human();
+  human.sayhello();
+  human.sayname();
+  final animal = Animal();
 }
 
 class Person {
@@ -63,7 +71,58 @@ class Person {
          person.setAge=45;   --- takes as a variable*/
 }
 
+// inheritance
 
+class Animal {
+  void sayhello() {
+    print('sayhello');
+  }
+}
+
+class Human extends Animal {
+  void sayname() {
+    print('sayname');
+  }
+
+  @override
+  void sayhello() {
+    print('hello human'); //prints hello human
+    super.sayhello(); // prints base class i.e sayhello
+  }
+}
+
+abstract class Animal1 {
+  //interface
+  void sayhello() {
+    ;
+  }
+}
+
+class Human1 implements Animal1 {
+  @override // if override not used sayhello() becomes functionof human1
+  void sayhello() {
+    print('hello');
+  }
+
+  final human1 = Human1();
+}
+
+// mixin used for multiple inheritance
+// can inherit from mixin with definitions like age =5,etc
+mixin Animal2 {
+  int age = 9;
+  void sayhey() {
+    print('animal2');
+  }
+}
+mixin Animal3 {
+  int age = 4;
+  void sayhey2() {
+    print('animal3');
+  }
+}
+
+class boy with Animal2, Animal3 {}
 
 
 
